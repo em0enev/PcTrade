38b536aca5d2ab98b2b1ab75525df91cbe4c0e03
Emil Enev <emil.enev@abv.bg>
2020-04-01 14:03:40 +0300

    add data and service layers

---
+++ PcTrade.Data/PcDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PcTrade.Data
+{
+    public class PcDbContext : IdentityDbContext
+    {
+        public PcDbContext(DbContextOptions<PcDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ PcTrade.Services/Interface/IAdService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PcTrade.Services.Interface
+{
+    class IAdService
+    {
+    }
+}

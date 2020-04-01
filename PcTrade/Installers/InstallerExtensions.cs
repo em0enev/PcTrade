7a6a65541e60002d7cafe35eff1c860f8c99075a
Emil Enev <emil.enev@abv.bg>
2020-04-01 14:05:15 +0300

    declaring InstallApplicationServices method

--- PcTrade/Installers/InstallerExtensions.cs
+++ PcTrade/Installers/InstallerExtensions.cs
@@ -17,5 +17,16 @@ namespace PcTrade.Installers
 
             installers.ForEach(installer => installer.InstallServices(configuration, services));
         }
+
+        public static void InstallApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var installers = typeof(Startup).Assembly.ExportedTypes
+                 .Where(type => typeof(IInstaller).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+                 .Select(Activator.CreateInstance)
+                 .Cast<IInstaller>()
+                 .ToList();
+
+            installers.ForEach(installer => installer.InstallServices(configuration, services));
+        }
     }
 }

545a7a58a432150261855cdac5ced932ec1b316f
Emil Enev <emil.enev@abv.bg>
2020-04-01 11:51:42 +0300

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PcTrade.Controllers
+{
+    public class TestController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "ivan" });
+        }
+    }
+}
